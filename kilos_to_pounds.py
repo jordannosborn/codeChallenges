7331e8946a392f1dbeede77115e3bbb7f3858c93
Jude Osborn <judeosborn@google.com>
2022-07-11 13:56:12 +1000

    Kilos to pounds

---
+++ kilos_to_pounds.py
@@ -0,0 +1,15 @@
+# Challenge: Kilos to Pounds
+# Create a function called kilos_to_pounds that converts a number from kilos to pounds.
+# The function should return a value that is rounded up to the nearest 2 decimal points. 
+# So rather than returning 5.91234 it should return 4.92.
+# Hint a pounds is 2.20462 kilos. Do NOT round this amount. Only round the final output of the function.
+# Hint: Python has a very simple function that takes care of the rounding.
+
+def kilos_to_pounds(x):
+  return 0
+
+# Tests -- don't delete anything below this line!
+assert kilos_to_pounds(200) == 440.92
+assert kilos_to_pounds(10) == 22.05
+assert kilos_to_pounds(50000) == 110231.0
+assert kilos_to_pounds(34) == 74.96
