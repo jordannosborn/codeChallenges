c16361665ab429e9186ca46e2046841235b76e98
Jude Osborn <judeosborn@google.com>
2022-07-15 14:56:43 +1000

    sum_arrays

---
+++ sum_array.py
@@ -0,0 +1,11 @@
+# Challenge: Sum Array
+# Create a function called sum_array that sums all the values of an array of integers.
+
+def sum_array(the_array):
+  return 0;
+
+# Tests -- don't delete anything below this line!
+assert sum_array([1, 1]) == 2
+assert sum_array([1, 2, 3, 4]) == 10
+assert sum_array([10, 100, 50]) == 160
+
