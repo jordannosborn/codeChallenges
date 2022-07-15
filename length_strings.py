82cd14d990428a4e53fd705c89aacaedf70ec4e3
Jude Osborn <judeosborn@google.com>
2022-07-15 15:09:40 +1000

    Added legnth strings

---
+++ length_strings.py
@@ -0,0 +1,11 @@
+# Challenge: Length Strings
+# Create a function called length_strings that accepts two strings and returns the length of both of them combined.
+
+def length_strings(string1, string2):
+  return 0
+
+
+# Tests:
+assert length_strings("cat", "dog") == 6
+assert length_strings("abcdefghijklmnopqrstuvwxy", "z") == 26
+assert length_strings("     ", " ") == 6
