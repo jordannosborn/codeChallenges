afaef941f0ea2fbce71bdb4068e44980a418d6dc
Jude Osborn <judeosborn@google.com>
2022-07-17 15:57:51 +1000

    Added ballin

---
+++ ballin.py
@@ -0,0 +1,12 @@
+# Challenge: Ballin
+# Create function called ballin that accepts an integer, multiplies it by 100 and converts it from USD dollars to Australian dollars.
+# Hint: 1 US dollar is 1.47 Australian dollars.
+
+def ballin(us_dollars):
+  return 0
+
+
+# Tests:
+assert ballin(1) == 147
+assert ballin(10) == 1470
+assert ballin(500) == 73500
