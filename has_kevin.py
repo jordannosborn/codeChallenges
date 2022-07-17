733ccc51e8e696a2172bc8f5b84519a05e81f73f
Jude Osborn <judeosborn@google.com>
2022-07-17 16:10:34 +1000

    Added has_kevin

---
+++ has_kevin.py
@@ -0,0 +1,13 @@
+# Challenge: Has Kevin
+# Create a function called has_kevin that returns True if a string has the substring "Kevin" in it and false if it doesn't.
+# Hint: A substring is part of a string. For example, "BCD" is a substring of "ABCDEFG" and "Kevin" is a substring of "Kevin Sean Osborn"
+
+def has_kevin(the_string):
+  return False
+
+# Tests -- don't delete anything below this line!
+assert has_kevin("Kevin Osborn") == True
+assert has_kevin("Jude Osborn") == False
+assert has_kevin("Kevin") == True
+assert has_kevin("blah blah blah") == False
+assert has_kevin("Where the fuck is Kevin?") == True
