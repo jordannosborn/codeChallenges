bbe2f15467c1d57f731e0c8b71a4e993e3ad2111
jordannosborn <jordannmosborn@gmail.com>
2022-07-18 15:54:19 +1000

    finished

--- has_kevin.py
+++ has_kevin.py
@@ -3,7 +3,11 @@
 # Hint: A substring is part of a string. For example, "BCD" is a substring of "ABCDEFG" and "Kevin" is a substring of "Kevin Sean Osborn"
 
 def has_kevin(the_string):
-  return False
+  substring = "Kevin"
+  if substring in the_string:
+    return True
+  else:
+    return False
 
 # Tests -- don't delete anything below this line!
 assert has_kevin("Kevin Osborn") == True
