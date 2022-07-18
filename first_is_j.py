fca907c4cf2c413b2f1812fd474d892a9aaa72e1
Jude Osborn <judeosborn@google.com>
2022-07-18 17:17:33 +1000

    Added first is j

---
+++ first_is_j.py
@@ -0,0 +1,11 @@
+# Challenge: First character is j
+# Create a function called first_is_j that returns True if the first character of a string is either lower case "j" or upper case "J".
+
+def first_is_j(the_string):
+  return False
+
+# Tests -- don't delete anything below this line!
+assert first_is_j("Jude") == True
+assert first_is_j("Dude") == False
+assert first_is_j("jelly") == True
+assert first_is_j("belly") == False
