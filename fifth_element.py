71dbaa24d9c4b6437657032a7b0a76b09a4b3b8d
Jude Osborn <judeosborn@google.com>
2022-07-26 13:36:58 +1000

    Added fifth element

---
+++ fifth_element.py
@@ -0,0 +1,16 @@
+# Challenge: Fifth element
+# Create a function called fifth_element that returns only the value of the fifth element of an array.
+# So if an array has five elements, it should return the last one. Or if it has six it should return the second to last.
+# If the array has less than five elements you should return "".
+# Hint: Don't forget that arrays are "zero indexed" that means they start with 0, not 1.
+
+
+def fifth_element(the_array):
+  return ""
+
+
+# Tests:
+assert fifth_element(["a", "b", "c", "d", "e"]) == "e"
+assert fifth_element(["Jude", "Amie", "Kevin", "Ana", "Jordann", "Kitty", "Charles", "Pickles"]) == "Jordann"
+assert fifth_element(["a"]) == ""
+assert fifth_element(["a", "b"]) == ""
--- jeepers_score.py
+++ jeepers_score.py
@@ -1,6 +1,6 @@
 # Challenge: Jeepers score
 # Create a function called jeepers_score that returns the number of times the word "jeepers" appears in a string.
-# The function should work regardless of upper or lower case. So "Jeepers" counts, as does "JEEPers" and "JeeperS".
+# The function should be case insensitive, which means it works regardless of upper or lower case. So "Jeepers" counts, as does "JEEPers" and "JeeperS".
 # Hint: Consider converting to lower case before counting.
 
 
