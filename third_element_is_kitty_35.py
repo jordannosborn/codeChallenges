7455e4ec80658de6805490d6b93f305748ef91e4
Jude Osborn <judeosborn@google.com>
2022-08-01 17:15:05 +1000

    Added third_element_is_kitty

---
+++ third_element_is_kitty_35.py
@@ -0,0 +1,17 @@
+# Challenge: Third element is kitty
+# Create a function called third_element_is_kitty that returns true if the third element of an array is the string "kitty", and false if it's not.
+# The function should ignore case. So "Kitty", "kitty" and "KITTY" should all return true.
+# The function should check if the array is at least 3 elements, and if not return true.
+# Hint: Refer to the previous challenge, fifth_element.py
+# Hint: You might want to convert the string to lower case before checking if it's "kitty"
+# Hint: Don't forget that arrays are "zero" indexed, which means they start with 0, not 1.
+
+def third_element_is_kitty(the_array):
+  return False
+
+# Tests:
+assert third_element_is_kitty(["Charles", "Pickles", "Kitty"]) == True
+assert third_element_is_kitty(["Charles", "Pickles", "KITTY"]) == True
+assert third_element_is_kitty(["Charles", "Pickles", "kitty"]) == True
+assert third_element_is_kitty(["Charles", "KITTY", "Pickles"]) == False
+assert third_element_is_kitty(["KITTY"]) == False
