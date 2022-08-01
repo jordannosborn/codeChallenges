e385080fc408573d4ddd9288d4ca4f40de19f3f7
Jude Osborn <judeosborn@google.com>
2022-08-01 17:01:48 +1000

    Added five to nine

---
+++ five_to_nine_30.py
@@ -0,0 +1,10 @@
+# Challenge: Five to nine
+# Create a function called five_to_nine that creates a string with the numbers from 5 to 9.
+# Hint: Consider range().
+# Hint: You might need to convert the integer to a string.
+
+def five_to_nine():
+  return ""
+
+# Tests:
+assert five_to_nine() == "56789"
