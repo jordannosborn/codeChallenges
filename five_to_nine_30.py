638b9130af1c2e99529c04daf2716db9e33ae669
Jude Osborn <judeosborn@google.com>
2022-08-21 23:24:47 -0500

    Added rule to five_to_nine

--- five_to_nine_30.py
+++ five_to_nine_30.py
@@ -1,5 +1,6 @@
 # Challenge: Five to nine
 # Create a function called five_to_nine that creates a string with the numbers from 5 to 9.
+# The function must use a loop. You can't just return "56789". :)
 # Hint: Consider range().
 # Hint: You might need to convert the integer to a string.
 
