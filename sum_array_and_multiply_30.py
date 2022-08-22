891e4b247a0e725a47669d93f433a8b324c2de83
Jude Osborn <judeosborn@google.com>
2022-08-21 23:18:15 -0500

    Revert "Revert "Added and moved some files""
    This reverts commit 1c570134e1dbdb2d6e241a0b76c034dc2884e316.

---
+++ sum_array_and_multiply_30.py
@@ -0,0 +1,11 @@
+# Challenge: Sum Array
+# Create a function called sum_array_and_multiply that sums all the values of 
+# an array of integers and then multiplies the sum by the parameter "mult".
+
+def sum_array(the_array, mult):
+  return 0
+
+
+# Tests -- don't delete anything below this line!
+assert sum_array([1, 1], 2) == 4
+assert sum_array([1, 2, 3, 4], 5) == 50
