52a6287f88d4be3dc5ba977ae41da30827268a73
jordannosborn <jordannmosborn@gmail.com>
2022-09-07 09:55:42 +1000

    finished

--- five_to_nine_30.py
+++ five_to_nine_30.py
@@ -2,7 +2,7 @@
 # Create a function called five_to_nine that creates a string with the numbers from 5 to 9.
 # The function must use a loop. You can't just return "56789". :)
 # Hint: Consider range().
-# Hint: You might need to convert the integer to a string.
+# Hint: You might need to convert the integer to a string.l
 
 def five_to_nine():
   output = ""
