87918d58350e1c443c4ae3ba7d9d90cc46e5556d
Jude Osborn <judeosborn@google.com>
2022-10-19 20:34:26 +1100

    Added reverse_and_print_array.py

---
+++ reverse_and_print_array_20.py
@@ -0,0 +1,20 @@
+# Challenge: Reverse and print array
+# Create a function called reverse_and_print_array that accepts an array as a
+# parameter, reverses the array and then prints each element one at a time.
+
+
+def print_array_except_jude(the_array):
+  print('')
+
+
+# Assuming an array like this:
+#
+# ["what", "the", "fuck"]
+#
+# The function should print this:
+#
+# fuck
+# the
+# what
+
+print_array_except_jude(["what", "the", "fuck"])
