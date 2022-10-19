a73c82864936682544af6dc327634c2b647278ef
Jude Osborn <judeosborn@google.com>
2022-10-19 20:28:43 +1100

    a few wording changes

--- print_any_to_any_15.py
+++ print_any_to_any_15.py
@@ -1,16 +1,13 @@
 # Challenge: Print any to any
-# Create a function called print_any_to_any accepts two numbers are parameters and prints those numbers in order, one line at a time.
+# Create a function called print_any_to_any accepts two numbers as parameters and prints those numbers in order, one line at a time.
 
 
 def print_any_to_any(num1, num2):
   print(0)
 
 
-# If you use 5 and 10 as parameters you'll need to be sure the output looks exactly like this:
+# If you use 5 and 7 as parameters you'll need to be sure the output looks exactly like this:
 # 5
 # 6
 # 7
-# 8
-# 9
-# 10
-print_any_to_any(5, 10)
+print_any_to_any(5, 7)
