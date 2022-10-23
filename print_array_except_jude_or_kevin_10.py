7e89f8eea6ed2ea71e0a148fa7fdae854a62c0b7
Jude Osborn <judeosborn@google.com>
2022-10-23 16:22:41 +1100

    Added print array except jude or kevin

---
+++ print_array_except_jude_or_kevin_10.py
@@ -0,0 +1,22 @@
+# Challenge: Print array except "Jude" or "Kevin"
+# Create a function called print_array_except_jude that prints each element of 
+# an array, unless that element is either "Jude" or "Kevin", in which case 
+# nothing is printed.
+
+
+def print_array_except_jude_or_kevin(the_array):
+  for x in the_array: 
+    if x != "Jude" and x != "Kevin":
+      print(x)
+    
+
+# Assuming an array like this:
+
+# ["Jordann", "Jude", "Kevin"]
+
+# The function should print this:
+
+# Amie
+# Jordann
+
+print_array_except_jude(["Amie", "Jordann", "Jude", "Kevin"])
