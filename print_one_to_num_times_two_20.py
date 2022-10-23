78c0af52c8f95825345030d11f13f44266968a2e
Jude Osborn <judeosborn@google.com>
2022-10-23 16:20:17 +1100

    print one to num times two

---
+++ print_one_to_num_times_two_20.py
@@ -0,0 +1,22 @@
+# Challenge: Print One to num times two
+# Create a function called print_one_to_num_times_two that accepts the
+# parameter num, and prints each number from 1 to num multiplied by two.
+# HINT: Don't forget that range needs 1 added to it in order to work 
+# as expected.
+# HINT: This is a print challenge, so you don't need to return anything.
+
+
+def print_one_to_num_times_two(num):
+  print(0)
+
+
+# If num is 3, the output should look exactly like this:
+#
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+
+print_one_to_num_times_two(3)
