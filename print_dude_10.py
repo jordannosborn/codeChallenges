06502df9d27d52bea45de190859c68e0e9e876bf
Jude Osborn <judeosborn@google.com>
2022-11-05 17:17:33 +1100

    added print dude

---
+++ print_dude_10.py
@@ -0,0 +1,18 @@
+# Challenge: Print dude
+# Create a function called print_dude that prints each element of 
+# an array as long as that element is "dude".
+
+
+def print_dude(the_array):
+  print('')
+    
+
+# Assuming an array like this:
+
+# ["yo", "sup", "dude"]
+
+# The function should print this:
+
+# dude
+
+print_dude(["yo", "sup", "dude"])
