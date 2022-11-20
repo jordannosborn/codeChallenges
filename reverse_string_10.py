6a646706b2d8ae455bf33088a11deaa71735f4a3
Jude Osborn <judeosborn@google.com>
2022-11-20 19:18:17 +1100

    Reverse string

---
+++ reverse_string_10.py
@@ -0,0 +1,11 @@
+# Challenge: Reverse a string
+# Create a function called reverse_string that returns a string that has been
+# reversed. So the string "jordann" should return "nnadroj".
+
+
+def reverse_string(the_string):
+  return ''
+
+
+assert reverse_string('jordann') == 'nnadroj'
+assert reverse_string('bud') == 'dub'
