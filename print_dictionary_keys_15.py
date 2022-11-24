a63cf51fe8f0b509de9a09749a035f1c3a46bd80
Jude Osborn <judeosborn@google.com>
2022-11-24 10:52:37 +1000

    print dict keys

---
+++ print_dictionary_keys_15.py
@@ -0,0 +1,26 @@
+# Challenge: Print dictionary keys
+# Create a function called print_dictionary_keys that prints only the keys of
+# a dictionary.
+#
+# Hint: Note that we are using Python 3. If you find an example for Python 2,
+# it may not work.
+
+
+def print_dictionary_keys(the_dictionary):
+  print('')
+
+
+# Test:
+
+# Assuming a dictionary like this:
+
+# {'name': 'Kevin', 'age': 22, 'number_of_pets': 2}
+
+# The function should print this:
+
+# name
+# age
+# number_of_pets
+
+
+print_dictionary_keys({'name': 'Kevin', 'age': 22, 'number_of_pets': 2})
