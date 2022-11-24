d3ebc93b900d77bd6086b4132997f646ebbab30a
Jude Osborn <judeosborn@google.com>
2022-11-24 10:20:51 +1000

    replace_array_jude_dude

---
+++ replace_jude_dude_15.py
@@ -0,0 +1,17 @@
+# Challenge: Replace jude with dude in an array
+# Create a function called replace_array_jude_dude that replaces all occurances of 
+# "jude" in array with "dude".
+#
+# Hint: It might be easy to get confused and think that this is about replacing
+# occurances of a substring in a string. That's NOT what this challenges is
+# about. Instead, the end result should be an array that is almost the same as 
+#, except that all occurances of "jude" are "dude". Look closely at the tests.
+
+
+def replace_array_jude_dude(the_array):
+  output = []
+  return output
+    
+
+assert(replace_array_jude_dude(['a', 'b', 'jude']) == ['a', 'b', 'dude'])
+assert(replace_array_jude_dude(['jordann', 'jude', 'amie']) == ['jordann', 'dude', 'amie'])
