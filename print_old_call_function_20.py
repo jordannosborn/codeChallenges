8a18edf732ce44d714701fe0439dfcafd17bb798
Jude Osborn <judeosborn@google.com>
2022-11-24 10:36:07 +1000

    make old

---
+++ print_old_call_function_20.py
@@ -0,0 +1,24 @@
+# Challenge: print old by calling a function
+# This challenges is about calling a function inside a function.
+# It requires two functions:
+# 1. make_old(age), which multiplies an age by 10. You'll need to create this
+# function yourself.
+# 2. print_old(age), which calls make_old, passing an age to it, then printing 
+# that age.
+
+
+def print_old(age):
+  print(0)
+
+
+# First, let's make sure you've made a function called make_old, and are
+# multiplying the age by 10.
+assert(make_old(10) == 100)
+assert(make_old(2) == 20)
+
+# Now let's see if print_old is calling make_old properly. If it is, then
+# print_old(12) should print this:
+
+# 120
+
+print_old(12)
