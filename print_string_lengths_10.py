b2d6b2a720bde394a82c78a95d241982d2ee8d52
Jude Osborn <judeosborn@google.com>
2022-11-24 10:24:02 +1000

    Fixed string leng

---
+++ print_string_lengths_10.py
@@ -0,0 +1,22 @@
+# Challenge: Print string lengths
+# Create a function called print_string_lengths that prints the length of each
+# string in an array
+
+
+def print_string_lengths(the_array):
+  print('')
+
+
+
+# Assuming an array like this:
+
+# ['abc', 'a', 'abcdefg']
+
+# The function should print this:
+
+# 3
+# 1
+# 7
+
+
+print_string_lengths(['abc', 'a', 'abcdefg'])
