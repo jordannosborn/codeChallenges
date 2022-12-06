e9f01afe6efa0ff7f10c6ac82cf54fff1cd6729b
Jude Osborn <judeosborn@google.com>
2022-12-06 20:34:15 +1100

    print except parents

---
+++ print_array_except_parents_10.py
@@ -0,0 +1,21 @@
+# Challenge: Print array except parents
+# Create a function called print_array_except_parents that prints each element
+# of an array, unless that element is either "Dad" or "Mum", in which case 
+# nothing is printed.
+
+
+def print_array_except_parents(the_array):
+  print('')
+
+
+# Assuming an array like this:
+#
+# ["Jordann", "Dad", "Kevin", "Mum"]
+#
+# The function should print this:
+#
+# Jordann
+# Kevin
+
+print_array_except_parents(["Jordann", "Dad", "Kevin", "Mum"])
+
