2a81bc9248ba5203f73523efe0d033431e2fa656
Jude Osborn <judeosborn@google.com>
2022-12-06 20:31:47 +1100

    print odd

---
+++ print_odd_10.py
@@ -0,0 +1,20 @@
+# Challenge: Print odd
+# Create a function called print_odd that prints each element of an
+# that is an odd number.
+
+
+def print_odd(the_array):
+  print(0)
+
+
+# Assuming an array like this:
+
+# [3, 4, 5, 6, 7]
+
+# The function should print this:
+
+# 3
+# 5
+# 7
+
+print_odd([3, 4, 5, 6, 7])
