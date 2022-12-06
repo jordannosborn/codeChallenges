7b7a46bb403f96bb7729330f57f4a9b4b0381f20
Jude Osborn <judeosborn@google.com>
2022-12-06 20:25:49 +1100

    print array with multiplier

---
+++ print_array_with_multiplier_15.py
@@ -0,0 +1,24 @@
+# Challenge: Print array with multiplier
+# Create a function called print_array_with_multiplier that prints each element
+# of an array of integers and multiplies it by a given integer.
+# 
+# Note that print_array_with_multiplier has two parameters: One is the array,
+# the other is the number to multiply by.
+
+
+def print_array_with_multiplier(the_array, mulitply_by):
+  for x in the_array:
+    print(x * mulitply_by)
+    
+
+# Assuming an array like this:
+
+# [3, 4, 10]
+
+# ...and a multiplier of 5, the function should print this:
+
+# 15
+# 20
+# 50
+
+print_array_with_multiplier([3, 4, 10], 5)
