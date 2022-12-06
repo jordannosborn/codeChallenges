69d7f6e12ff534a273e2e96e8eda6136e7bb314e
Jude Osborn <judeosborn@google.com>
2022-12-06 20:40:47 +1100

    is even

---
+++ is_even_10.py
@@ -0,0 +1,11 @@
+# Challenge: Is the number even?
+# Create a function called is_even that returns True if a given integer is
+# even and False if it's not.
+
+
+def is_even(num):
+  return False
+
+
+assert(is_even(2) == True)
+assert(is_even(3) == False)
