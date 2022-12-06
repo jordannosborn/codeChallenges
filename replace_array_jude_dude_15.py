90272a18d018d42945c2ec65da25240fdf477edd
jordannosborn <jordannmosborn@gmail.com>
2022-12-06 21:02:14 +1100

    finished

--- replace_array_jude_dude_15.py
+++ replace_array_jude_dude_15.py
@@ -5,13 +5,18 @@
 # Hint: It might be easy to get confused and think that this is about replacing
 # occurances of a substring in a string. That's NOT what this challenges is
 # about. Instead, the end result should be an array that is almost the same as 
-#, except that all occurances of "jude" are "dude". Look closely at the tests.
-
+# except that all occurances of "jude" are "dude". Look closely at the tests.
 
 def replace_array_jude_dude(the_array):
   output = []
+  for x in the_array:
+    if x == 'jude':
+      output.append('dude')
+    else:
+      output.append(x)
   return output
-    
+
 
 assert(replace_array_jude_dude(['a', 'b', 'jude']) == ['a', 'b', 'dude'])
 assert(replace_array_jude_dude(['jordann', 'jude', 'amie']) == ['jordann', 'dude', 'amie'])
+
