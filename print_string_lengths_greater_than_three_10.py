88ca19a3f488851afd5a77322e4550eaf6330871
Jude Osborn <judeosborn@google.com>
2022-12-06 20:16:07 +1100

    print string lenghts greater than 3

---
+++ print_string_lengths_greater_than_three_10.py
@@ -0,0 +1,19 @@
+# Challenge: Print string lengths greater than 3
+# Create a function called print_string_lengths_greater_than_three that prints 
+# the length of each, but only if it's greater than 3.
+
+
+def print_string_lengths_greater_than_three(the_array):
+  print(0)
+
+
+# Assuming an array like this:
+
+# ['abcd', 'a', 'abcdefg']
+
+# The function should print this:
+
+# 4
+# 7
+
+print_string_lengths_greater_than_three(['abcd', 'a', 'abcdefg'])
