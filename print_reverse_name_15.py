5d4092854bf5ecac4a67ef802eb685fb8aa6530e
Jude Osborn <judeosborn@google.com>
2022-12-06 20:12:26 +1100

    Print reverse string by calling a function

---
+++ print_reverse_name_15.py
@@ -0,0 +1,28 @@
+# Challenge: print reverse name by calling a function
+# This challenge is about calling a function inside a function.
+#
+# It requires two functions:
+#
+# 1. reverse_string(the_string), which accepts a string and returns the 
+# string reversed. You'll need to create this function yourself.
+#
+# 2. print_name(name), which calls reverse_string, passing the name to it and
+# prints the string reversed.
+
+def print_name(name):
+  print('')
+
+
+# Tests:
+
+# First, let's make sure you've made a function called reverse_string that
+# returns the reverse of a string.
+assert(reverse_string('Jude') == 'eduJ')
+assert(reverse_string('Jordann') == 'nnadroJ')
+
+# Now let's see if print_name is calling reverse_string properly. If it is, then
+# print_name('Jude') should print this:
+
+# eduJ
+
+print_name('Jude')
