3d692154e3c91a44fbc82a5d08fc712e6df1f086
Jude Osborn <judeosborn@google.com>
2022-12-09 10:49:01 +1100

    count families with Kevin

---
+++ count_families_with_kevin_35.py
@@ -0,0 +1,20 @@
+# Challenge: Count families with Kevin
+# Create a function called count_families_with_kevin that counts the number of
+# familes that have a Kevin. 
+# count_families_with_kevin() will accept a parameter that is an array of
+# families, so this means you will need to deal with a 2 dimensional array
+# (aka an array of arrays). 
+#
+# Hint: Keep in mind that in Python an "Array" is often referred to as a "List". Same 
+# thing.
+# Hint: This link might help you:
+# https://medium.com/an-amygdala/how-to-iterate-through-a-2d-list-in-python-5a90693f3a15
+
+
+def count_families_with_kevin(families):
+  return 0
+
+
+# Tests:
+assert count_families_with_kevin([['Kevin', 'Jude', 'Amie', 'Jordann'], ['Bob', 'Sara', 'Jane'], ['Doug', 'Kevin', 'Raj']]) == 2
+assert count_families_with_kevin([['Bob', 'Sara', 'Jane'], ['Doug', 'Raj']]) == 0
