c01793e7d64f805496fa6ab3c4cbc18290f0a2f0
Jude Osborn <judeosborn@google.com>
2022-12-08 17:06:41 +1100

    total challenges allowance

---
+++ total_challenge_allowance_35.py
@@ -0,0 +1,14 @@
+# Challenge: Total challenge allowance.
+# Create a function called total_challenge_allowance that accepts an array of
+# challenge filenames, extracts the allowance reward and adds them all together
+# to return the total.
+#
+# Hint: Consider split()
+
+def total_challenge_allowance(challenges):
+  return 0
+
+
+# Tests:
+assert total_challenge_allowance(['make_stuff_10', 'has_kitty_15', 'print_all_kevins_10']) == 35
+assert total_challenge_allowance(['divisible_by_ten_20', 'count_dudes_5', 'print_except_jude_25']) == 5
