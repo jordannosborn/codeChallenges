dca0c7e866279132252ddc3b90a28ef5c9f8a039
Jude Osborn <judeosborn@google.com>
2022-12-09 10:35:14 +1100

    added calorie counter

---
+++ calorie_counter_15.py
@@ -0,0 +1,11 @@
+# Challenge: Calorie counter.
+# Create a function called calorie_counter that accepts a dictionary of foods
+# and their calories, and returns the total calories.
+
+def calorie_counter(foods):
+  return 0
+
+
+# Tests:
+assert calorie_counter({'milk': 150, 'skim-milk': 100, 'almond-milk': 100}) == 350
+assert calorie_counter({'eggs': 160, 'bacon': 200, 'toast': 80, 'coffee': 0}) == 440
