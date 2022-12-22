a35e83276f4920d9b37a3687e235749293ce4a9c
Jude Osborn <judeosborn@gmail.com>
2022-12-22 13:15:28 +1100

    Added count judes

---
+++ count_judes_15.py
@@ -0,0 +1,20 @@
+# Challenge: Count Judes
+# Create a function called count_judes that accepts and array of families and
+# counts the number of people named "Jude".
+#
+# HINT: Some Families may have more than one "Jude", so this is not about 
+# counting the numbe of families with Jude, but rather than total number of 
+# Judes in all families.
+
+def count_judes(families):
+  return 0
+
+# Tests:
+assert count_judes(
+  [
+    ['Kevin', 'Jude', 'Amie', 'Jordann'], 
+    ['Jude', 'Bob', 'Sara', 'Jane', 'Jude'], 
+    ['Doug', 'Kevin', 'Raj']
+  ]
+) == 3
+assert count_judes([['Bob', 'Sara', 'Jane'], ['Doug', 'Raj']]) == 0
