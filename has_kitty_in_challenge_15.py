92251ab43ff825e5039fe2d03463f42f3109a0da
Jude Osborn <judeosborn@gmail.com>
2022-12-22 13:07:07 +1100

    Added has kitty in challenge

---
+++ has_kitty_in_challenge_15.py
@@ -0,0 +1,15 @@
+# Challenge: Has kitty in challenge
+# Create a function called has_kitty_in_challenge that accepts an array of
+# challenge filenames and returns true of any of them have the word "kitty" in
+# them.
+#
+# HINT: As soon as "kitty" is found you can return True. No need to cycle through
+# all the other elements in the array.
+
+
+def has_kitty_in_challenge(challenges):
+  return False
+
+# Tests:
+assert has_kitty_in_challenge(['make_stuff_10', 'has_kitty_15', 'print_all_kevins_10']) == True
+assert has_kitty_in_challenge(['divisible_by_ten_20', 'count_dudes_5', 'print_except_jude_25']) == False
